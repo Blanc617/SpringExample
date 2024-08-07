880ab1bd6f5ebbe84bbb0a5da765e98c5cae864f
jinusong14@gmail.com <jinusong14@gmail.com>
2024-08-07 21:40:25 +0900

    [송진우] - DB(Mybatis) 연동 예제 문제

---
+++ src/main/java/com/jinu/spring/ex/database/repository/UsedGoodsRepository.java
@@ -0,0 +1,17 @@
+package com.jinu.spring.ex.database.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.jinu.spring.ex.database.domain.UsedGoods;
+
+// 데이터베이스 관련 처리
+// 쿼리 수행
+@Mapper
+public interface UsedGoodsRepository {
+	
+	// usedGoods 모든 행 조회
+	public List<UsedGoods> selectUserGoodsList();
+
+}
